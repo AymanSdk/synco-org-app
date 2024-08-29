20230c558dfe536064e0e3b95fd7b3922e57b25c
Aymansdk <ayman.sdk123@outlook.com>
2024-08-29 14:09:23 +0100

    feat: Add workspaces module and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,25 @@
 
 import { UserButton } from "@/features/auth/components/user-button";
 
+import { useEffect, useMemo } from "react";
+
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+
 export default function Home() {
+  const { data, isLoading } = useGetWorkspaces();
+
+  const workspaceId = useMemo(() => data?.[0]?._id, [data]);
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    if (workspaceId) {
+      console.log("Redirect to workspace");
+    } else {
+      console.log("Open creation modal");
+    }
+  }, [workspaceId, isLoading]);
+
   return (
     <div>
       <UserButton />
