3ba31ccb1f9adbd7fe31857567ed5bd6f9524514
Aymansdk <ayman.sdk123@outlook.com>
2024-09-02 23:15:41 +0100

    feat: Add Sidebar and Toolbar components to WorkspaceIdLayout
    Enhance the WorkspaceIdLayout component by adding a Sidebar component and a Toolbar component to the layout. This change improves the user experience by providing a sidebar for additional navigation and functionality within the workspace page, as well as a toolbar with search and info buttons for improved navigation and accessibility.

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,16 +1,6 @@
-"use client";
-
-import { useWorkspaceId } from "@/hooks/use-workspace-id";
-import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
-
 const WorkspaceIdPage = () => {
-  const workspaceId = useWorkspaceId();
-  const { data } = useGetWorkspace({ id: workspaceId });
-
   return (
-    <div className="text-lg text-rose-800 underline">
-      Data : {JSON.stringify(data)}
-    </div>
+    <div className="text-lg text-rose-800 underline">WorkSpace Id Page</div>
   );
 };
 
--- src/app/workspace/[workspaceId]/workspace-switcher.tsx
+++ src/app/workspace/[workspaceId]/workspace-switcher.tsx
@@ -1,4 +1,4 @@
-import { Loader } from "lucide-react";
+import { Loader, Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -53,10 +53,24 @@ export const WorkspaceSwitcher = () => {
         {filteredWorkspaces?.map((workspace) => (
           <DropdownMenuItem
             key={workspace._id}
-            className="cursor-pointer capitalize"
-            onClick={() => router.push(`/worksapce/${workspace._id}`)}
-          ></DropdownMenuItem>
+            className="cursor-pointer capitalize overflow-hidden"
+            onClick={() => router.push(`/workspace/${workspace._id}`)}
+          >
+            <div className="shrink-0 size-9 relative overflow-hidden bg-[#616061] text-white font-semibold text-lg rounded-md flex items-center justify-center mr-2">
+              {workspace.name.charAt(0).toUpperCase()}
+            </div>
+            <p className="truncate">{workspace.name}</p>
+          </DropdownMenuItem>
         ))}
+        <DropdownMenuItem
+          className="cursor-pointer"
+          onClick={() => setOpen(true)}
+        >
+          <div className="size-9 relative overflow-hidden bg-[#F2F2F2] text-slate-800 font-semibold text-lg rounded-md flex items-center justify-center mr-2">
+            <Plus />
+          </div>
+          Create a new workspace
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
