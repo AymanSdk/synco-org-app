df796d26ce09f036ce18cbaa08012c066564f76a
Aymansdk <ayman.sdk123@outlook.com>
2024-09-03 09:44:44 +0100

    feat: Enhance WorkspaceIdLayout with Sidebar and Toolbar components
    Improve the WorkspaceIdLayout component by adding a Sidebar component and a Toolbar component to the layout. This change enhances the user experience by providing a sidebar for additional navigation and functionality within the workspace page, as well as a toolbar with search and info buttons for improved navigation and accessibility.

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -1,6 +1,15 @@
 import { v } from "convex/values";
-import { mutation, query } from "./_generated/server";
 import { auth } from "./auth";
+import { mutation, query } from "./_generated/server";
+
+const GenerateCode = () => {
+  const code = Array.from(
+    { length: 6 },
+    () => "0123456789abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 36)]
+  ).join("");
+
+  return code;
+};
 
 export const create = mutation({
   args: {
@@ -12,13 +21,19 @@ export const create = mutation({
     if (!userId) {
       throw new Error("Unauthorized");
     }
-    // TODO: create a proper method to generate join code
-    const joinCode = "123456";
+
+    const joinCode = GenerateCode();
 
     const workspaceId = await ctx.db.insert("workspaces", {
       name: args.name,
       userId,
       joinCode,
+    });
+
+    await ctx.db.insert("members", {
+      userId,
+      workspaceId,
+      role: "admin",
     });
 
     return workspaceId;
@@ -28,7 +43,30 @@ export const create = mutation({
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query("workspaces").collect();
+    const userId = await auth.getUserId(ctx);
+
+    if (!userId) {
+      return [];
+    }
+
+    const members = await ctx.db
+      .query("members")
+      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .collect();
+
+    const workspaceIds = members.map((member) => member.workspaceId);
+
+    const workspaces = [];
+
+    for (const workspaceId of workspaceIds) {
+      const workspace = await ctx.db.get(workspaceId);
+
+      if (workspace) {
+        workspaces.push(workspace);
+      }
+    }
+
+    return workspaces;
   },
 });
 
@@ -41,6 +79,17 @@ export const getById = query({
 
     if (!userId) {
       throw new Error("Unauthorized");
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId)
+      )
+      .unique();
+
+    if (!member) {
+      return null;
     }
 
     return await ctx.db.get(args.id);
