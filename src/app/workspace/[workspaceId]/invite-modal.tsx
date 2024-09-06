16906f2ece5af63e4963ae2dfdb17a0c223b6ad6
Aymansdk <ayman.sdk123@outlook.com>
2024-09-06 19:35:07 +0100

    feat: Add useNewJoinCode hook for generating new join codes

--- src/app/workspace/[workspaceId]/invite-modal.tsx
+++ src/app/workspace/[workspaceId]/invite-modal.tsx
@@ -1,6 +1,6 @@
 import { toast } from "sonner";
 
-import { CopyIcon } from "lucide-react";
+import { CopyIcon, RefreshCcw } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -12,6 +12,9 @@ import {
 } from "@/components/ui/dialog";
 
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { useNewJoinCode } from "@/features/workspaces/api/use-new-join-code";
+import { DialogClose } from "@radix-ui/react-dialog";
+import { useConfirm } from "@/hooks/use-confirm";
 
 interface InviteModalProps {
   open: boolean;
@@ -27,6 +30,30 @@ export const InviteModal = ({
   joinCode,
 }: InviteModalProps) => {
   const workspaceId = useWorkspaceId();
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure ?",
+    "This will disable the current invite code and generate a new one. All previous invite links will be invalid."
+  );
+
+  const { mutate, isPending } = useNewJoinCode();
+
+  const handleNewCode = async () => {
+    const ok = await confirm();
+
+    if (!ok) return;
+
+    mutate(
+      { workspaceId },
+      {
+        onSuccess: () => {
+          toast.success("New invite code generated");
+        },
+        onError: () => {
+          toast.error("Failed to generate new invite code");
+        },
+      }
+    );
+  };
 
   const handleCopy = () => {
     const inviteLink = `${window.location.origin}/join/${workspaceId}`;
@@ -37,24 +64,40 @@ export const InviteModal = ({
   };
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Invite peers to {name}</DialogTitle>
-          <DialogDescription>
-            Use the code below to invite peers to your workspace.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="flex flex-col gap-y-4 items-center justify-center py-10">
-          <p className="text-4xl font-bold tracking-widest uppercase">
-            {joinCode}
-          </p>
-          <Button onClick={handleCopy} variant="ghost" size="sm">
-            Copy link
-            <CopyIcon className="size-4 ml-2" />
-          </Button>
-        </div>
-      </DialogContent>
-    </Dialog>
+    <>
+      <ConfirmDialog />
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Invite peers to {name}</DialogTitle>
+            <DialogDescription>
+              Use the code below to invite peers to your workspace.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex flex-col gap-y-4 items-center justify-center py-10">
+            <p className="text-4xl font-bold tracking-widest uppercase">
+              {joinCode}
+            </p>
+            <Button onClick={handleCopy} variant="ghost" size="sm">
+              Copy link
+              <CopyIcon className="size-4 ml-2" />
+            </Button>
+          </div>
+          <div className="flex items-center justify-between w-full">
+            <Button
+              disabled={isPending}
+              onClick={handleNewCode}
+              variant="outline"
+            >
+              New code
+              <RefreshCcw className="size-4 ml-2" />
+            </Button>
+            <DialogClose asChild>
+              <Button className="bg-[#5C3B58]">Close</Button>
+            </DialogClose>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
