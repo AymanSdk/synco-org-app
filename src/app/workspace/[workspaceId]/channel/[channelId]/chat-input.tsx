7e68aea52f9b36281251dd032d19ef2e5a6d134c
Aymansdk <ayman.sdk123@outlook.com>
2024-09-08 10:12:26 +0100

    Refactor ChatInput component to include submit functionality and handle message creation

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,7 +1,14 @@
-import dynamic from "next/dynamic";
 import Quill from "quill";
+import dynamic from "next/dynamic";
 
-import { useRef } from "react";
+import { useCreateMessage } from "@/features/messages/api/use-create-message";
+
+import { useChannelId } from "@/hooks/use-channel-id";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+
+import { toast } from "sonner";
+
+import { useRef, useState } from "react";
 
 const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
 
@@ -10,24 +17,49 @@ interface ChatInputProps {
 }
 
 export const ChatInput = ({ placeholder }: ChatInputProps) => {
+  const [editorKey, setEditorKey] = useState(0);
+  const [isPending, setIsPending] = useState(false);
+
   const editorRef = useRef<Quill | null>(null);
 
-  const handleSumbit = ({
+  const workspaceId = useWorkspaceId();
+  const channelId = useChannelId();
+
+  const { mutate: createMessage } = useCreateMessage();
+
+  const handleSumbit = async ({
     body,
     image,
   }: {
     body: string;
     image: File | null;
   }) => {
-    console.log({ body, image });
+    try {
+      setIsPending(true);
+      await createMessage(
+        {
+          workspaceId,
+          channelId,
+          body,
+        },
+        { throwError: true }
+      );
+
+      setEditorKey((prevKey) => prevKey + 1);
+    } catch (error) {
+      toast.error("Failed to send message");
+    } finally {
+      setIsPending(false);
+    }
   };
 
   return (
     <div className="px-5 w-full">
       <Editor
+        key={editorKey}
         placeholder={placeholder}
         onSubmit={handleSumbit}
-        disabled={false}
+        disabled={isPending}
         innerRef={editorRef}
       />
     </div>
