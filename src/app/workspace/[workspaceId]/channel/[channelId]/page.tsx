23542c94a2a35538d01fe5c09be704edf711a7ba
Aymansdk <ayman.sdk123@outlook.com>
2024-09-10 08:13:30 +0100

    Refactor ChnnelIdPage component to use MessageList component for displaying messages

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -9,18 +9,17 @@ import { useChannelId } from "@/hooks/use-channel-id";
 
 import { Header } from "./header";
 import { ChatInput } from "./chat-input";
+import { MessageList } from "@/components/message-list";
 
 const ChnnelIdPage = () => {
   const channelId = useChannelId();
 
-  const { results } = useGetMessages({ channelId });
+  const { results, status, loadMore } = useGetMessages({ channelId });
   const { data: channel, isLoading: channelLoading } = useGetChannel({
     id: channelId,
   });
 
-  console.log(results);
-
-  if (channelLoading) {
+  if (channelLoading || status === "LoadingFirstPage") {
     return (
       <div className="h-full flex-1 flex items-center justify-center">
         <Loader className="size-5 animate-spin text-muted-foreground" />
@@ -40,7 +39,14 @@ const ChnnelIdPage = () => {
   return (
     <div className="flex flex-col h-full">
       <Header title={channel.name} />
-      <div className="flex-1">{JSON.stringify(results)}</div>
+      <MessageList
+        channelName={channel.name}
+        channelCreationTime={channel._creationTime}
+        data={results}
+        loadMore={loadMore}
+        isLoadingMore={status === "LoadingMore"}
+        canLoadMore={status === "CanLoadMore"}
+      />
       <ChatInput placeholder={`Message # ${channel.name}`} />
     </div>
   );
